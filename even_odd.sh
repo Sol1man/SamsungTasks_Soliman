df1b0ca42e0b04633eb5d9577214c3256598f2b0
nadsproj <fullimeprojwork@gmail.com>
2023-08-23 01:46:49 +0300

    Add conditional check for odd number

--- even_odd.sh
+++ even_odd.sh
@@ -6,4 +6,6 @@ read -p "Enter a number: " num
 # Check if the number is even or odd using conditional statements
 if ((num % 2 == 0)); then
     echo "$num is even."
-
+else
+    echo "$num is odd."
+fi
