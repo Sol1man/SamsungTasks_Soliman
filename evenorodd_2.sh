00a4c75747d48fec27ad7b120eaa320813345178
Booddda <ab.galal254@gmail.com>
2023-08-23 10:07:38 +0200

    Regex even or odd finder added

--- evenorodd_2.sh
+++ evenorodd_2.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo -n "Even numbers in the file: "
+
+#the idea is to decide whether the number is even or odd based on the last digit which will reduce compiling
+
+
+echo -n "Even numbers in the file: " >> newfile.txt
 grep -Eo '(^| )1?[0-9]?[02468]( |$)' input.txt >> newfile.txt
 
-echo -n "Odd numbers in the file: "
+echo -n "Odd numbers in the file: " >> newfile.txt
 grep -Eo '(^| )1?[0-9]?[13579]( |$)' input.txt >> newfile.txt
